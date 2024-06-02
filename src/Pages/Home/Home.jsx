a29e6db0ab8774ec2c0a6732cfd9482c2c72053e
Andrew2maged <andrew30maged@gmail.com>
2024-06-02 15:30:23 +0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
                               <div className="item item-first">
                                    <div className="caption">
                                         <div className="col-md-offset-1 col-md-10">
-                                             <img className="" src={img1} />
+                                             {/* <img className="" src={img1} /> */}
                                              <h3>Let's make your life happier</h3>
                                              <h1>Healthy Living</h1>
                                              <a href="#team" className="section-btn btn btn-default smoothScroll">Meet Our Doctors</a>
