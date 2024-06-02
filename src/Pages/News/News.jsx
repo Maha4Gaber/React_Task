b20514724001dde93b7a189a098658e1fc843624
Rania244 <raniaa7med2092017@gmail.com>
2024-06-02 14:49:28 +0300

    Update News.jsx

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,86 +1,59 @@
-import React from 'react';
+import React, { useState ,useEffect} from 'react';
 import img1 from '../../assets/images/news-image1.jpg';
 import img2 from '../../assets/images/news-image2.jpg';
 import img3 from '../../assets/images/news-image3.jpg';
 import auther from '../../assets/images/author-image.jpg';
-
+import axios from 'axios';
 
 const News = () => {
+  const [news, setnews] = useState([]);
+ 
+        useEffect(() => {
+          const getdata= async()=>{
+               try{
+                 await axios.get("http://localhost:3000/news").then((res)=>{
+                   // console.log(res.data);
+                   setnews(res.data)
+                 });
+           
+               }catch(error){
+                 console.error("Error fetching data:",error);
+               }
+             };
+             
+          getdata()
+        }, []);
   return (
       <section id="news" data-stellar-background-ratio="2.5">
           <div class="container">
                <div class="row">
-
-                    <div class="col-md-12 col-sm-12">
+                   <div class="col-md-12 col-sm-12">
                          {/* <!-- SECTION TITLE --> */}
                          <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
                               <h2>Latest News</h2>
                          </div>
                     </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         {/* <!-- NEWS THUMB --> */}
-                         <div class="news-thumb wow fadeInUp" data-wow-delay="0.4s">
-                              <a href="news-detail.html">
-                                        <img src={img1} class="img-responsive" alt=""></img>
-                              </a>
-                              <div class="news-info">
-                                   <span>March 08, 2018</span>
-                                   <h3><a href="news-detail.html">About Amazing Technology</a></h3>
-                                   <p>Maecenas risus neque, placerat volutpat tempor ut, vehicula et felis.</p>
-                                   <div class="author">
-                                        <img src={auther} class="img-responsive" alt=""></img>
-                                        <div class="author-info">
-                                             <h5>Jeremie Carlson</h5>
-                                             <p>CEO / Founder</p>
-                                        </div>
-                                   </div>
-                              </div>
+                    {news.map((newsItem, index) => (
+                     <div className="col-md-4 col-sm-6" key={index}>
+                     <div className="news-thumb wow fadeInUp" data-wow-delay={`${0.4 + index * 0.2}s`}>
+                       <a href="news-detail.html">
+                         <img src={news.image} className="img-responsive" alt="" />
+                       </a>
+                       <div className="news-info">
+                         <span>{news.date}</span>
+                         <h3><a href="news-detail.html">{news.title}</a></h3>
+                         <p>{news.description}</p>
+                         <div className="author">
+                           <img src={news.authorImage} className="img-responsive" alt="" />
+                           <div className="author-info">
+                             <h5>{news.author}</h5>
+                             <p>{news.role}</p>
+                           </div>
                          </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         {/* <!-- NEWS THUMB --> */}
-                         <div class="news-thumb wow fadeInUp" data-wow-delay="0.6s">
-                              <a href="news-detail.html">
-                                   <img src={img2} class="img-responsive" alt=""></img>
-                              </a>
-                              <div class="news-info">
-                                   <span>February 20, 2018</span>
-                                   <h3><a href="news-detail.html">Introducing a new healing process</a></h3>
-                                   <p>Fusce vel sem finibus, rhoncus massa non, aliquam velit. Nam et est ligula.</p>
-                                   <div class="author">
-                                        <img src={auther}  class="img-responsive" alt=""></img>
-                                        <div class="author-info">
-                                             <h5>Jason Stewart</h5>
-                                             <p>General Director</p>
-                                        </div>
-                                   </div>
-                              </div>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         {/* <!-- NEWS THUMB --> */}
-                         <div class="news-thumb wow fadeInUp" data-wow-delay="0.8s">
-                              <a href="news-detail.html">
-                                   <img src={img3} class="img-responsive" alt=""></img>
-                              </a>
-                              <div class="news-info">
-                                   <span>January 27, 2018</span>
-                                   <h3><a href="news-detail.html">Review Annual Medical Research</a></h3>
-                                   <p>Vivamus non nulla semper diam cursus maximus. Pellentesque dignissim.</p>
-                                   <div class="author">
-                                        <img src={auther}  class="img-responsive" alt=""></img>
-                                        <div class="author-info">
-                                             <h5>Andrio Abero</h5>
-                                             <p>Online Advertising</p>
-                                        </div>
-                                   </div>
-                              </div>
-                         </div>
-                    </div>
-
+                       </div>
+                     </div>
+                   </div>
+                    ))}
                </div>
           </div>
      </section>
