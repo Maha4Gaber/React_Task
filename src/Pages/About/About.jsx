d64d38a95885a958ebee29205aa62618849be318
Andrew2maged <andrew30maged@gmail.com>
2024-06-02 14:56:35 +0300

    Update About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
+// import img1 from '../../assets/images/about-bg.jpg'
 
 const About = () => {
   return (
-    <section id="about">
-          <div class="container">
-               <div class="row">
+    <div className="container">
+               <div className="row">
 
-                    <div class="col-md-6 col-sm-6">
-                         <div class="about-info">
-                              <h2 class="wow fadeInUp" data-wow-delay="0.6s">Welcome to Your <i class="fa fa-h-square"></i>ealth Center</h2>
-                              <div class="wow fadeInUp" data-wow-delay="0.8s">
+                    <div className="col-md-6 col-sm-6">
+                         <div className="about-info">
+                              <h2 className="wow fadeInUp" data-wow-delay="0.6s">Welcome to Your <i className="fa fa-h-square"></i>ealth Center</h2>
+                              <div className="wow fadeInUp" data-wow-delay="0.8s">
                                    <p>Aenean luctus lobortis tellus, vel ornare enim molestie condimentum. Curabitur lacinia nisi vitae velit volutpat venenatis.</p>
                                    <p>Sed a dignissim lacus. Quisque fermentum est non orci commodo, a luctus urna mattis. Ut placerat, diam a tempus vehicula.</p>
                               </div>
-                              <figure class="profile wow fadeInUp" data-wow-delay="1s">
-                                   <img src="images/author-image.jpg" class="img-responsive" alt="">
+                              <figure className="profile wow fadeInUp" data-wow-delay="1s">
+                                   <img src="images/author-image.jpg" className="img-responsive" alt=""/>
                                    <figcaption>
                                         <h3>Dr. Neil Jackson</h3>
                                         <p>General Principal</p>
@@ -25,7 +25,7 @@ const About = () => {
                     
                </div>
           </div>
-     </section>
+  
   );
 }
 
